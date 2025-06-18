8eff9ea39180aae5a89426746fd5f4161f484627
Matheus Tavares <mt937169@gmail.com>
2025-06-17 22:13:54 -0300

    Game loop

--- src/com/DeadKeymon449/rain/Game.java
+++ src/com/DeadKeymon449/rain/Game.java
@@ -1,23 +1,32 @@
 package com.DeadKeymon449.rain;
 
 public class Game implements Runnable {
-	
+
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
-	
+
 	private Thread thread;
-	
-	public synchronized void start() { 
+	private boolean running = false;
+
+	public synchronized void start() {
+		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-	
-	public synchronized void stop() { 
+
+	public synchronized void stop() {
+		running = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		while (running == true) {
+			
 		}
 	}
 }
