486c9d07b2da373056382dff6a47b74631da2ea0
Matheus Tavares <mt937169@gmail.com>
2025-07-08 16:48:17 -0300

    Timer

--- src/com/DeadKeymon449/rain/Game.java
+++ src/com/DeadKeymon449/rain/Game.java
@@ -53,8 +53,20 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void run() {
+		long lastTime = System.nanoTime();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0.0;
+
 		while (running == true) {
-			tick();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			while (delta >= 1) {
+				tick();
+				delta--;
+			}
+
 			render();
 		}
 	}
